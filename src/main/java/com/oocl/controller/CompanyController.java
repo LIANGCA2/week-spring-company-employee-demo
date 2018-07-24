ad0272f8cc9e2267e254ee5216fcc41be8f31291
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-24 22:00:41 +0800

    finish conceptMap

--- src/main/java/com/oocl/controller/CompanyController.java
+++ src/main/java/com/oocl/controller/CompanyController.java
@@ -1,14 +1,27 @@
 package com.oocl.controller;
 
 
+import com.oocl.model.Company;
+import com.oocl.model.Employee;
 import com.oocl.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CompanyController {
 
     @Autowired
     private CompanyService companyService;
+
+
+    @GetMapping("/companies")
+    @ResponseBody
+    public List<Company> findAllCompany() {
+        companyService.findAllCompany();
+    }
 
 }
